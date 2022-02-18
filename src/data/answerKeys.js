2b20e3be86dcc1eeac888b7a1ac5aa731c55db96
luptak <luptak.michal22@gmail.com>
2022-02-18 15:34:55 +0100

    fix answers to 2012-6306

--- src/data/answerKeys.js
+++ src/data/answerKeys.js
@@ -11,18 +11,149 @@ export const answerKeys = [
       {
         number: 2,
         type: "long",
-        answers: ["13.6"],
+        answers: ["31944"],
       },
       {
         number: 3,
         type: "long",
-        answers: ["13.6"],
+        answers: ["3.5"],
       },
       {
         number: 4,
+        type: "long",
+        answers: ["40"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["3"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["70"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["-6.25"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["54.24"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["15"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["15.95"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["0.12","0.87"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["5"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["22.8"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["350"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["241.66"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["173"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["9.6"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["37"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["540"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["2"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "",
+      },
+      {
+        number: 22,
         type: "select",
         answer: "A",
       },
+      {
+        number: 23,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "C",
+      },
+
     ],
   },
 ];
