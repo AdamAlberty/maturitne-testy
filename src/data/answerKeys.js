8d8c7a04099cf82f089ec9d23c5b0503244da9dc
luptak <luptak.michal22@gmail.com>
2022-02-18 16:28:22 +0100

    add 2011

--- src/data/answerKeys.js
+++ src/data/answerKeys.js
@@ -313,4 +313,162 @@ export const answerKeys = [
       },
     ]
   },
+  {
+    id: 3,
+    slug: "2011-3306",
+    name: "Maturitný test 2011 - 3306",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["15"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["336"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["34"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["36"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["-2011"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["90"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["-40"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["36"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["4.24"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["348"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["700"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["0.11"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["6"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["35.26"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["5"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["294"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["242"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["2"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["10"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["3"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "D",
+      },
+
+    ]
+  },
 ];
