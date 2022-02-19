3b4bf1e9e473339e4db07311ca2ab79110d0e498
AdamAlberty <alberty.adam@gmail.com>
2022-02-19 11:54:11 +0100

    Add answers to the rest of the tests

--- src/data/answerKeys.js
+++ src/data/answerKeys.js
@@ -17,7 +17,7 @@ export const answerKeys = [
       {
         number: 3,
         type: "long",
-        answers: ["3.5"],
+        answers: ["3,5"],
       },
       {
         number: 4,
@@ -37,12 +37,12 @@ export const answerKeys = [
       {
         number: 7,
         type: "long",
-        answers: ["-6.25"],
+        answers: ["-6,25"],
       },
       {
         number: 8,
         type: "long",
-        answers: ["54.24"],
+        answers: ["54,24"],
       },
       {
         number: 9,
@@ -52,12 +52,12 @@ export const answerKeys = [
       {
         number: 10,
         type: "long",
-        answers: ["15.95"],
+        answers: ["15,95"],
       },
       {
         number: 11,
         type: "long",
-        answers: ["0.12","0.87"],
+        answers: ["0,12", "0,87"],
       },
       {
         number: 12,
@@ -67,7 +67,7 @@ export const answerKeys = [
       {
         number: 13,
         type: "long",
-        answers: ["22.8"],
+        answers: ["22,8"],
       },
       {
         number: 14,
@@ -77,7 +77,7 @@ export const answerKeys = [
       {
         number: 15,
         type: "long",
-        answers: ["241.66"],
+        answers: ["241,66"],
       },
       {
         number: 16,
@@ -87,7 +87,7 @@ export const answerKeys = [
       {
         number: 17,
         type: "long",
-        answers: ["9.6"],
+        answers: ["9,6"],
       },
       {
         number: 18,
@@ -169,12 +169,12 @@ export const answerKeys = [
       {
         number: 2,
         type: "long",
-        answers: ["78.4"],
+        answers: ["78,4"],
       },
       {
         number: 3,
         type: "long",
-        answers: ["47.75"],
+        answers: ["47,75"],
       },
       {
         number: 4,
@@ -184,12 +184,12 @@ export const answerKeys = [
       {
         number: 5,
         type: "long",
-        answers: ["16.5"],
+        answers: ["16,5"],
       },
       {
         number: 6,
         type: "long",
-        answers: ["117.28"],
+        answers: ["117,28"],
       },
       {
         number: 7,
@@ -224,7 +224,7 @@ export const answerKeys = [
       {
         number: 13,
         type: "long",
-        answers: ["15.63"],
+        answers: ["15,63"],
       },
       {
         number: 14,
@@ -311,7 +311,7 @@ export const answerKeys = [
         type: "select",
         answer: "B",
       },
-    ]
+    ],
   },
   {
     id: 3,
@@ -361,7 +361,7 @@ export const answerKeys = [
       {
         number: 9,
         type: "long",
-        answers: ["4.24"],
+        answers: ["4,24"],
       },
       {
         number: 10,
@@ -376,7 +376,7 @@ export const answerKeys = [
       {
         number: 12,
         type: "long",
-        answers: ["0.11"],
+        answers: ["0,11"],
       },
       {
         number: 13,
@@ -386,7 +386,7 @@ export const answerKeys = [
       {
         number: 14,
         type: "long",
-        answers: ["35.26"],
+        answers: ["35,26"],
       },
       {
         number: 15,
@@ -468,7 +468,948 @@ export const answerKeys = [
         type: "select",
         answer: "D",
       },
-
-    ]
+    ],
   },
-];
+  {
+    id: 4,
+    slug: "2015-1203",
+    name: "Maturitný test 2015 - 1203",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["15"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["96"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["72"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["9"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["9,27"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["-5"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["108"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["0,8", "0,80"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["102"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["25"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["17,43"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["-2"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["33,33"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["126"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["0,57"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["3"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["780"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["0,85"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["18"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["47,89"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "B",
+      },
+    ],
+  },
+  {
+    id: 5,
+    slug: "2014-2106",
+    name: "Maturitný test 2014 - 2106",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["26"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["14"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["-7"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["8"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["-8"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["2697,74", "2697,75"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["9372"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["4"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["35,26"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["3"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["243"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["8"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["-4"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["21"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["102"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["5"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["25"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["10"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["3"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["67"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "D",
+      },
+    ],
+  },
+  {
+    id: 6,
+    slug: "2017-7180",
+    name: "Maturitný test 2017 - 7180",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["2,92"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["23,94"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["14325"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["1231,50"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["6,51"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["34"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["0,33"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["-0,87"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["11"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["-1"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["39"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["16"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["1"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["11"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["265,85"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["7"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["105"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["0,22"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["0,24"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["0,41"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "B",
+      },
+    ],
+  },
+  {
+    id: 7,
+    slug: "2016-5178",
+    name: "Maturitný test 2016 - 5178",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["5,4"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["18"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["56"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["-2,73"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["15"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["1,25"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["0,75"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["0,25"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["5"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["4"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["175"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["24"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["25"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["30"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["49"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["0,43"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["45"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["48"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["89"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["27,94"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "C",
+      },
+    ],
+  },
+  {
+    id: 8,
+    slug: "2018-7070",
+    name: "Maturitný test 2018 - 7070",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["-5"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["9"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["85"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["9,5"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["4"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["9,5"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["8"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["3,14"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["7,07"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["5000"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["124"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["7"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["17,43"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["0,5"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["3,2"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["12"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["214,67"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["23,07"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["14,56"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["58,91"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "C",
+      },
+    ],
+  },
+  {
+    id: 9,
+    slug: "2019-191447",
+    name: "Maturitný test 2019 - 191447",
+    questions: [
+      {
+        number: 1,
+        type: "long",
+        answers: ["7,5"],
+      },
+      {
+        number: 2,
+        type: "long",
+        answers: ["55"],
+      },
+      {
+        number: 3,
+        type: "long",
+        answers: ["-4"],
+      },
+      {
+        number: 4,
+        type: "long",
+        answers: ["8"],
+      },
+      {
+        number: 5,
+        type: "long",
+        answers: ["48"],
+      },
+      {
+        number: 6,
+        type: "long",
+        answers: ["891"],
+      },
+      {
+        number: 7,
+        type: "long",
+        answers: ["25"],
+      },
+      {
+        number: 8,
+        type: "long",
+        answers: ["2"],
+      },
+      {
+        number: 9,
+        type: "long",
+        answers: ["1,5"],
+      },
+      {
+        number: 10,
+        type: "long",
+        answers: ["1,2"],
+      },
+      {
+        number: 11,
+        type: "long",
+        answers: ["279,75"],
+      },
+      {
+        number: 12,
+        type: "long",
+        answers: ["52,5"],
+      },
+      {
+        number: 13,
+        type: "long",
+        answers: ["3"],
+      },
+      {
+        number: 14,
+        type: "long",
+        answers: ["25,56"],
+      },
+      {
+        number: 15,
+        type: "long",
+        answers: ["2"],
+      },
+      {
+        number: 16,
+        type: "long",
+        answers: ["53,13"],
+      },
+      {
+        number: 17,
+        type: "long",
+        answers: ["4"],
+      },
+      {
+        number: 18,
+        type: "long",
+        answers: ["2,48"],
+      },
+      {
+        number: 19,
+        type: "long",
+        answers: ["36,31"],
+      },
+      {
+        number: 20,
+        type: "long",
+        answers: ["0,13"],
+      },
+      {
+        number: 21,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 22,
+        type: "select",
+        answer: "A",
+      },
+      {
+        number: 23,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 24,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 25,
+        type: "select",
+        answer: "D",
+      },
+      {
+        number: 26,
+        type: "select",
+        answer: "C",
+      },
+      {
+        number: 27,
+        type: "select",
+        answer: "B",
+      },
+      {
+        number: 28,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 29,
+        type: "select",
+        answer: "E",
+      },
+      {
+        number: 30,
+        type: "select",
+        answer: "A",
+      },
+    ],
+  },
+]
